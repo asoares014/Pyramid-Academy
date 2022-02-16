e4b5b676458ed1fd08ed28c3bed6194e86360a31
Andrea Soares <andrea.soares014@gmail.com>
2022-02-16 11:17:33 -0500

    updated with try catch

--- GuessTheNumber/src/GuessTheNumber.java
+++ GuessTheNumber/src/GuessTheNumber.java
@@ -1,49 +1,39 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(100) + 1;
+        //System.out.println(randomNumber);
 
         System.out.println("Hello! What is your name?");
         String name = input.nextLine();
-        System.out.println("Well, " + name + " , I am thinking of a number between 1 and 20.");
-        int number;
-        int count = 0;
-        do {
-            System.out.println("Take a guess");
-            number = input.nextInt();
-            count++;
-            if (number > 14) {
-                System.out.println("Your guess is too high");
-            }
-            if (number < 14) {
-                System.out.println("Your guess is too low");
-            } else if (number == 14) {
-                System.out.println("Good job, " + name + " You guessed my number in " + count + " guesses!");
-            }
-        } while (number != 14);
-        System.out.println("Would you like to play again? (y or n)");
+        System.out.println("Well, " + name + " , I am thinking of a number between 1 and 100.");
+        System.out.println("Enter your guess:");
+        int counter = 0;
 
-        Scanner again = new Scanner(System.in);
-        String playAgain = again.nextLine();
-        if(playAgain.equals("y")) {
-            System.out.println("Let's play again!..I am thinking of a number between 1 and 20");
-            int countNew = 0;
-            do {
-                System.out.println("Take a guess!");
-                number = input.nextInt();
-                countNew++;
-                if (number > 9) {
-                    System.out.println("Your guess is too high");
+        while (true) {
+            try {
+                int playerGuess = input.nextInt();
+                counter++;
+
+                if (playerGuess == randomNumber) {
+                    System.out.println("Correct! You Win!");
+                    System.out.println("You guessed my number in " + counter + " tries!");
+                    break;
+                } else if (randomNumber > playerGuess) {
+                    System.out.println("Your guess is too low! Guess again");
+                } else {
+                    System.out.println("Your guess is too high! Guess again");
                 }
-                if (number < 9) {
-                    System.out.println("Your guess is too low");
-                } else if (number == 9) {
-                    System.out.println("Good job, you guessed my number in " + countNew + " guesses!");
-                }
-            } while (number != 9);
-        }if (playAgain.equals("n")) {
-            System.out.println("See you next time!");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Invalid Input");
+                break;
+            }
         }
     }
 }
+
